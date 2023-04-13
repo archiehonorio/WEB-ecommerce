f23ea08544ae304e052d46c67c63635ed315c426
archiehonorio <archiehonorio404@gmail.com>
2023-04-13 17:51:47 +0800

    whole code

---
+++ components/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:09103244763"><i class="fas fa-phone"></i> +639103244763</a>
+         <a href="mailto:nozero40404@gmail.com"><i class="fas fa-envelope"></i>nozero40404@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Pasig, Philippines - 1600 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">   <p>For portfolio use</p><br>&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+
+</footer>
